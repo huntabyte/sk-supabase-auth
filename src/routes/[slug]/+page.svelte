953faf202d93f14c478b089003e77d5e4525279c
Soft-Dev-Fire <96921536+Soft-Code-Fire@users.noreply.github.com>
2023-02-18 14:11:11 -0500

    change 11

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores'
+
+    const route = $page.url.href;
+</script>
+
+<main>
+    <h1 class="text-3xl">Looks Like you came across a page that is not available! Maybe you can rewrite The Url?</h1>
+    <p>The current route is : {route}</p>
+</main>
+
+<style>
+
+</style>
